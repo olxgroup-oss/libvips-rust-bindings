f01822551ca9bbf85cb874a15e7c17f561f29aec
Andrew Basson <andrew.basson@olx.com>
2023-11-02 12:31:08 +0100

    chore: publish version 1.6.0

--- generator/compile.sh
+++ generator/compile.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-cc -g -Wall introspect.c -o ./introspect `pkg-config vips --cflags --libs`
+# shellcheck disable=SC2046
+cc -g -Wall introspect.c -o ./introspect $(pkg-config vips --cflags --libs)
--- generator/generate.sh
+++ generator/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -v $(pwd):/src -v $(pwd)/../src/:/bindings -e CARGO_HTTP_MULTIPLEXING=false -e RUSTFLAGS='-C target-feature=-crt-static' -e BINDINGS_DIR=/bindings -w /src -it libvips-builder cargo build
+docker run --rm -v ./:/src -v ./../src/:/bindings -e CARGO_HTTP_MULTIPLEXING=false -e RUSTFLAGS='-C target-feature=-crt-static' -e BINDINGS_DIR=/bindings -w /src -it libvips-builder cargo build
