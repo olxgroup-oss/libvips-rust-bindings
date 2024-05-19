96df86cc5a3391f05f80534f4de01d5eb90dc760
lotusshinoaki <tatsuya@lotusandcompanyinc.com>
2024-05-20 08:57:49 +0900

    Use cfg macro

--- build.rs
+++ build.rs
@@ -1,12 +1,18 @@
 // (c) Copyright 2019-2024 OLX
 fn main() {
-    //
-    // Get the binaries for Windows from the link below.
-    // https://github.com/libvips/build-win64-mxe/releases/
-    //
-    // Use Windows binaries with the suffix `-ffi.zip` .
-    //
-    println!("cargo:rustc-link-lib=libvips");
-    println!("cargo:rustc-link-lib=libglib-2.0");
-    println!("cargo:rustc-link-lib=libgobject-2.0");
+    if cfg!(target_os = "windows") {
+        //
+        // Get the binaries for Windows from the link below.
+        // https://github.com/libvips/build-win64-mxe/releases/
+        //
+        // Use Windows binaries with the suffix `-ffi.zip` .
+        //
+        println!("cargo:rustc-link-lib=libvips");
+        println!("cargo:rustc-link-lib=libglib-2.0");
+        println!("cargo:rustc-link-lib=libgobject-2.0");
+    } else {
+        println!("cargo:rustc-link-lib=vips");
+        println!("cargo:rustc-link-lib=glib-2.0");
+        println!("cargo:rustc-link-lib=gobject-2.0");
+    }
 }
